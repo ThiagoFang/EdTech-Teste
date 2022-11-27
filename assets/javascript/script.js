fe8f66800786974f992ac89f969efb4b7bc2b588
Thiago <thiagofang@gmail.com>
2022-11-27 18:39:02 -0300

    dropdown section

---
+++ assets/javascript/script.js
@@ -0,0 +1,19 @@
+/*    DROPDOWN AREA */
+const dropDownButtonList = document.querySelectorAll('.toggle__button.dropdown');
+
+const toggleDropDownStatus = (key) => {
+  const dropDownDivList = document.querySelectorAll('.dropdown__box-item');
+  const buttonImg = dropDownDivList[key].querySelector('.toggle__button img');
+  dropDownDivList[key].classList.toggle('show')
+
+  if(dropDownDivList[key].classList.contains('show')) {
+    buttonImg.src = "./assets//icons/icon-arrow-up.svg"
+    return
+  };
+  buttonImg.src = "./assets//icons/icon-arrow-down.svg";
+};
+
+dropDownButtonList.forEach((item) => {
+  const dataKey = item.dataset.key;
+  item.addEventListener('click', () => toggleDropDownStatus(dataKey));
+});
